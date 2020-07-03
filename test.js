f5af9c82c2645eb40a52d8c259741615bffe4010
Wanderson <wandersonsouza@trixlog.com>
2020-07-03 10:08:54 -0300

    0.4.1

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var numberLocale = require('./src/index');
 
-console.log(numberLocale.toLocaleString(1000.0));
+console.log(numberLocale.toLocaleString(1000.09, null, {minimumFractionDigits: 1}));
 console.log(numberLocale.toLocaleString(10000, 'pt-BR'));
 console.log(numberLocale.toLocaleString('1024.30', 'pt-BR'));
 console.log(numberLocale.toLocaleString('1024.300000', 'pt-BR', { style: 'currency', currency: 'brl', minimumFractionDigits: 2 }));
